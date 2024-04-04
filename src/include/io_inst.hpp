cd12dde63a21feea6a3573bab4ee2a5c42b018ef
vabarbosa <vitor.alves@mindsight.com.br>
2024-04-04 19:27:44 -0300

    feat/read specification part

--- src/include/io_inst.hpp
+++ src/include/io_inst.hpp
@@ -11,6 +11,12 @@
 class IData{
 public:
     std::string instance_name;
+    int n_nodes;
+    std::string edge_weight_type;
+    std::string edge_weight_format;
+    std::string edge_data_format;
+    std::string node_coord_type;
+    std::string display_data_type;
     std::vector<int> nodes;
     std::vector<std::vector<long double>> distances;
 
--- src/lib/io_inst.cpp
+++ src/lib/io_inst.cpp
@@ -1,15 +1,53 @@
 #include "io_inst.hpp"
 
-void IData::read_input(Parameters& param) {
-	std::ifstream file;
-	file.open(param.filename);
-	if (!file) exit(1);
+void IData::read_input(Parameters &param)
+{
+    std::ifstream file;
+    file.open(param.filename);
+    if (!file)
+        exit(1);
 
-	std::string aux;
+    std::string aux;
     file >> aux;
-	assert(aux == "NAME:");
-	file >> this->instance_name;
-    std::cout << this->instance_name << std::endl;
-		
-	file.close();
+    assert(aux == "NAME:");
+    file >> this->instance_name;
+
+    file >> aux;
+    assert(aux == "TYPE:");
+    file >> aux;
+    assert(aux == "TSP");
+
+    while (true){
+        file >> aux;
+        if (aux == "DIMENSION:"){
+            assert(aux == "DIMENSION:");
+            file >> this->n_nodes;
+        } else if (aux == "EDGE_WEIGHT_TYPE:"){
+            assert(aux == "EDGE_WEIGHT_TYPE:");
+            file >> this->edge_weight_type;
+        } else if (aux == "EDGE_WEIGHT_FORMAT:"){
+            assert(aux == "EDGE_WEIGHT_FORMAT:");
+            file >> this->edge_weight_format;
+        } else if (aux == "EDGE_DATA_FORMAT:"){
+            assert(aux == "EDGE_DATA_FORMAT:");
+            file >> this->edge_data_format;
+        } else if (aux == "NODE_COORD_TYPE:"){
+            assert(aux == "NODE_COORD_TYPE:");
+            file >> this->node_coord_type;
+        } else if (aux == "DISPLAY_DATA_TYPE:"){
+            assert(aux == "DISPLAY_DATA_TYPE:");
+            file >> this->display_data_type;
+        } else if( aux == "EOF"){
+            break;
+        }
+    }
+
+    std::cout << this->n_nodes << std::endl;
+    std::cout << this->edge_weight_type << std::endl;
+    std::cout << this->edge_weight_format << std::endl;
+    std::cout << this->edge_data_format << std::endl;
+    std::cout << this->node_coord_type << std::endl;
+    std::cout << this->display_data_type << std::endl;
+
+    file.close();
 }
