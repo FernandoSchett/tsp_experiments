5ea597518ca6a50686e1ab83a7a49fd468210deb
vabarbosa <vitor.alves@mindsight.com.br>
2024-04-17 19:27:43 -0300

    feat/run heur 50 iterations and change some chosen instances

--- run_heurs_chosen_insts.sh
+++ run_heurs_chosen_insts.sh
@@ -6,6 +6,10 @@ if [ -f "output.txt" ]; then
     rm "output.txt"
 fi
 
+echo "---------------" >> results/time_result.txt
+date +"%T" >> results/time_result.txt
+
+
 for i in $inst
 do
     ./TSP -f  $i -c nn_heur -i 50 >> output.txt
