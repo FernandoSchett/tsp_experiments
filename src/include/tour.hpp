3dfe7fe211a8929d141d004744c4a7b3369aead4
Fernando <fernandoschettini@outlook.com>
2024-04-17 13:42:34 -0300

    [CHORE]: remobing sabing heur

--- src/include/tour.hpp
+++ src/include/tour.hpp
@@ -13,7 +13,6 @@ public:
     int sol_value;
 
     void double_sided_nn_heur(IData& inst, Parameters& params);
-    void saving_heur(IData& inst, Parameters& params);
     void nn_heur(IData& inst, Parameters& params);
     bool is_tour_valid(IData& inst);
     void print_tour();
