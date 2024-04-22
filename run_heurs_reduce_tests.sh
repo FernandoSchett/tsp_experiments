bde96957680606233ff88b4d41be2bd34995736b
Fernando <fernandoschettini@outlook.com>
2024-04-21 23:14:14 -0300

    [FEAT]: new scripts for tests 2

--- run_heurs_reduce_tests.sh
+++ run_heurs_reduce_tests.sh
@@ -16,11 +16,14 @@ for i in $inst
 do
     for j in $alpha
     do
+        echo "./TSP -f $i -c multist_semi_nn_heur -m alpha --alpha=$j --seed=5 -i 100 >> testes_reduzidos.txt"
         ./TSP -f $i -c multist_semi_nn_heur -m alpha --alpha=$j --seed=5 -i 100 >> testes_reduzidos.txt
+         
     done
 
     for j in $k
     do
+        echo "./TSP -f $i -c multist_semi_nn_heur -m k_best --k=$j --seed=5 -i 100 >> testes_reduzidos.txt"
         ./TSP -f $i -c multist_semi_nn_heur -m k_best --k=$j --seed=5 -i 100 >> testes_reduzidos.txt
     done
 done
