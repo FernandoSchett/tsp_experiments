0f89202eaa75cbbfc99fb8a7418db4a19e6bc46e
vabarbosa <vitor.alves@mindsight.com.br>
2024-05-08 23:05:22 -0300

    refactor/script first imprv

--- scripts/run_ls_two_opt_first_imprv_chosen_insts.sh
+++ scripts/run_ls_two_opt_first_imprv_chosen_insts.sh
@@ -3,21 +3,21 @@ inst=./benchmark/instances/chosen_inst/*.tsp
 make clean
 make 
 
-mkdir benchmark/logs/local_search_first_improvement
-mkdir results/local_search_first_improvement
-rm benchmark/logs/local_search_first_improvement/*.tsp.txt
+mkdir benchmark/logs/local_search_two_opt_first_imprv
+mkdir results/local_search_two_opt_first_imprv
+rm benchmark/logs/local_search_two_opt_first_imprv/*.tsp.txt
 
-echo "--------------- Running local_search" >> ./results/local_search_first_improvement/time_result.txt
-date >> ./results/local_search_first_improvement/time_result.txt
-echo $USER >> ./results/local_search_first_improvement/time_result.txt
+echo "--------------- Running local_search" >> ./results/local_search_two_opt_first_imprv/time_result.txt
+date >> ./results/local_search_two_opt_first_imprv/time_result.txt
+echo $USER >> ./results/local_search_two_opt_first_imprv/time_result.txt
 
 for i in $inst
 do
     inst_name=$( basename $i ) 
-    echo "./TSP -f  $i -l nn_heur_i1 -c local_search -r two_opt_first_imprv >> benchmark/logs/local_search_first_improvement/$inst_name.txt" 
-    ./TSP -f  $i -l nn_heur_i1 -c local_search -r two_opt_first_imprv >> benchmark/logs/local_search_first_improvement/$inst_name.txt
+    echo "./TSP -f  $i -l nn_heur_i1 -c local_search -r two_opt_first_imprv | tee -a benchmark/logs/local_search_two_opt_first_imprv/$inst_name.txt" 
+    ./TSP -f  $i -l nn_heur_i1 -c local_search -r two_opt_first_imprv | tee -a benchmark/logs/local_search_two_opt_first_imprv/$inst_name.txt
 done
 
-date >> ./results/local_search_first_improvement/time_result.txt
-echo $USER >> ./results/local_search_first_improvement/time_result.txt
-echo "---------------" >> ./results/local_search_first_improvement/time_result.txt
+date >> ./results/local_search_two_opt_first_imprv/time_result.txt
+echo $USER >> ./results/local_search_two_opt_first_imprv/time_result.txt
+echo "---------------" >> ./results/local_search_two_opt_first_imprv/time_result.txt
