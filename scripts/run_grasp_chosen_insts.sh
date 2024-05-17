04e5329f8a25e42afc21a7a4609407fd59ab2d83
vabarbosa <vitor.alves@mindsight.com.br>
2024-05-17 00:28:13 -0300

    feat/grasp

---
+++ scripts/run_grasp_chosen_insts.sh
@@ -0,0 +1,28 @@
+cd ..
+inst=./benchmark/instances/chosen_inst/*
+make clean
+make 
+
+time="3600"
+alp="0.050000"
+s=5
+
+mkdir benchmark/logs/grasp_t${time}_a${alp}_s${s}
+mkdir results/grasp_t${time}_a${alp}_s${s}
+
+rm benchmark/logs/grasp_t${time}_a${alp}_s${s}/*.tsp.txt
+
+echo "--------------- Running GRASP with two_opt_first_imprv_circ_search and alpha = $alp" >> ./results/grasp_t${time}_a${alp}_s${s}/time_result.txt
+date >> ./results/grasp_t${time}_a${alp}_s${s}/time_result.txt
+echo $USER >> ./results/grasp_t${time}_a${alp}_s${s}/time_result.txt
+
+for i in $inst
+do
+    inst_name=$( basename $i ) 
+    echo "./TSP -f  $i -p time -t $time -c grasp -m alpha --a=$alp --seed=$s -r two_opt_first_imprv_circ_search >> benchmark/logs/grasp_t${time}_a${alp}_s${s}/$inst_name.txt" 
+    ./TSP -f  $i -p time -t $time -c grasp -m alpha --a=$alp --seed=$s -r two_opt_first_imprv_circ_search >> benchmark/logs/grasp_t${time}_a${alp}_s${s}/$inst_name.txt
+done
+
+date >> ./results/grasp_t${time}_a${alp}_s${s}/time_result.txt
+echo $USER >> ./results/grasp_t${time}_a${alp}_s${s}/time_result.txt
+echo "---------------" >> ./results/grasp_t${time}_a${alp}_s${s}/time_result.txt
--- src/lib/choice_method.cpp
+++ src/lib/choice_method.cpp
@@ -47,6 +47,14 @@ void run_choice_method(Tour& best_tour, IData& idata, Parameters& params, CPUTim
 		best_tour.read_solution_file(idata, params);
 		local_search(best_tour, idata, params, cpu_time);
 	}
+	else if (params.choice_method == "grasp"){
+		if (idata.n_nodes <= 100){
+			params.maxtime /= 4;
+		}else if(idata.n_nodes <= 1500){
+			params.maxtime /= 2;
+		}
+		grasp(best_tour, idata, params, cpu_time, randmt);
+	}
 	else {
 		printf("ERROR: No choice method selected.\n");
 		exit(0);
@@ -69,7 +77,6 @@ bool is_stop_criterion_satsfied(Parameters& params, double t, int i) {
 
 void multist_semi_nn_heur(Tour& best_tour, IData& idata, Parameters& params, CPUTime& cpu_time, std::mt19937& randmt) {
 	best_tour.nn_heur(idata, params);
-	best_tour.calc_tour_cost(idata);
 	printf("%d\n", best_tour.sol_value);
 
 	int i = 0;
@@ -78,7 +85,6 @@ void multist_semi_nn_heur(Tour& best_tour, IData& idata, Parameters& params, CPU
 	while (!is_stop_criterion_satsfied(params, cpu_time.s_CPU_during - cpu_time.s_CPU_inicial, i)) {
 		Tour tour;
 		tour.semi_nn_heur(idata, params, randmt);
-		tour.calc_tour_cost(idata);
 
 		if (tour.sol_value < best_tour.sol_value) {
 			best_tour = tour;
@@ -94,7 +100,6 @@ void multist_semi_nn_heur(Tour& best_tour, IData& idata, Parameters& params, CPU
 
 void multist_semi_dsnn_heur(Tour& best_tour, IData& idata, Parameters& params, CPUTime& cpu_time, std::mt19937& randmt) {
 	best_tour.double_sided_nn_heur(idata, params);
-	best_tour.calc_tour_cost(idata);
 	printf("%d\n", best_tour.sol_value);
 
 	int i = 0;
@@ -103,7 +108,6 @@ void multist_semi_dsnn_heur(Tour& best_tour, IData& idata, Parameters& params, C
 	while (!is_stop_criterion_satsfied(params, cpu_time.s_CPU_during - cpu_time.s_CPU_inicial, i)) {
 		Tour tour;
 		tour.semi_double_sided_nn_heur(idata, params, randmt);
-		tour.calc_tour_cost(idata);
 
 		if (tour.sol_value < best_tour.sol_value) {
 			best_tour = tour;
@@ -137,4 +141,30 @@ void local_search(Tour& best_tour, IData& idata, Parameters& params, CPUTime& cp
 	}
 
 	return;
+}
+
+void grasp(Tour& best_tour, IData& idata, Parameters& params, CPUTime& cpu_time, std::mt19937& randmt) {
+	get_cpu_time(&cpu_time.s_CPU_inicial, &cpu_time.s_total_inicial);
+	get_cpu_time(&cpu_time.s_CPU_during, &cpu_time.s_total_during);
+
+	best_tour.nn_heur(idata, params);
+	printf("%d\n", best_tour.sol_value);
+	printf("bestsol;currsol;semi_nn_sol_value;currtime;iteration\n");
+	int i = 0;
+	while (!is_stop_criterion_satsfied(params, cpu_time.s_CPU_during - cpu_time.s_CPU_inicial, i)) {
+		Tour tour;
+		tour.semi_nn_heur(idata, params, randmt);
+		int semi_greedy_sol_value = tour.sol_value;
+		local_search(tour, idata, params, cpu_time);
+
+		if (tour.sol_value < best_tour.sol_value) {
+			best_tour = tour;
+		}
+
+		get_cpu_time(&cpu_time.s_CPU_during, &cpu_time.s_CPU_inicial);
+		printf("%d;%d;%d;%lf;%d\n", best_tour.sol_value, tour.sol_value, semi_greedy_sol_value, cpu_time.s_CPU_during - cpu_time.s_CPU_inicial, i);
+		i++;
+	}
+	get_cpu_time(&cpu_time.s_CPU_final, &cpu_time.s_total_final);
+	cpu_time.total_s_CPU += (cpu_time.s_CPU_final - cpu_time.s_CPU_inicial);
 }
