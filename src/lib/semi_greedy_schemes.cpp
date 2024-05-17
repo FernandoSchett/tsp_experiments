e9de1b91ad8ec5fad3e350546be7fabd7fbb3e7c
vabarbosa <vitor.alves@mindsight.com.br>
2024-05-16 23:27:31 -0300

    feat/format file semi_greedy_schemes

--- src/lib/semi_greedy_schemes.cpp
+++ src/lib/semi_greedy_schemes.cpp
@@ -4,7 +4,7 @@
 Candidate cardinality_scheme(std::vector<Candidate>& cl, Parameters& params, std::mt19937& randmt) {
     int k = std::min(params.k_best, int(cl.size()));
     int idx_candidate = randmt() % k;
-    std::partial_sort(cl.begin(), cl.begin()+k, cl.end(), rcl_dist_lte_cmp); // O(n log k)
+    std::partial_sort(cl.begin(), cl.begin() + k, cl.end(), rcl_dist_lte_cmp); // O(n log k)
     Candidate chosen_candidate = cl[idx_candidate];
     return chosen_candidate;
 }
@@ -13,14 +13,14 @@ Candidate quality_scheme(std::vector<Candidate>& cl, Parameters& params, std::mt
     // O(n)
     int c_min = INT_MAX;
     int c_max = 0;
-    for(int i=0; i<cl.size(); i++){
+    for (int i = 0; i < cl.size(); i++) {
         c_min = std::min(c_min, cl[i].dist);
         c_max = std::max(c_max, cl[i].dist);
     }
     int max_dist_allowed = c_min + params.alpha * (c_max - c_min);
     std::vector<Candidate> rcl;
-    for(int i=0; i<cl.size(); i++){
-        if(cl[i].dist <= max_dist_allowed){
+    for (int i = 0; i < cl.size(); i++) {
+        if (cl[i].dist <= max_dist_allowed) {
             rcl.push_back(cl[i]);
         }
     }
