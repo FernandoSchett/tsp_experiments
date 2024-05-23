63ac8da181101db3d0438976f8b37f2b046698f1
vabarbosa <vitor.alves@mindsight.com.br>
2024-05-23 15:34:12 -0300

    feat/script to combine results grasp loor4 tttplos

---
+++ scripts/combine_results_grasp_reduced_tests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd ..
+inst=("kroA100.tsp" "si1032.tsp")
+look4=("21282" "92650") 
+time="600"
+alphas="0.010000 0.025000 0.050000 0.100000"
+
+mkdir tttplots
+
+for alp in $alphas
+do
+    for s in $(seq 1 50)
+    do
+        dir_with_time="grasp_t${time}_a${alp}_s${s}"
+        dir="grasp_a${alp}_s${s}"
+        awk -v var=";${s}" '{print $0 var}' "./results/${dir}/time_result.txt" > "./results/${dir}/tmp.txt"
+        cat ./results/${dir}/tmp.txt | grep kroA100 >> tttplots/time_result.txt
+        cat ./results/${dir}/tmp.txt | grep si1032 >> tttplots/time_result.txt
+        rm ./results/${dir}/tmp.txt
+    done
+done
+
